5429b790c8d08aa3e1f95207936f187e625c3e7b
Shreya Gupta <shreyakgupta09@gmail.com>
2025-12-29 21:12:46 +0530

    Time: 1470 ms (24.21%), Space: 48.6 MB (33.12%) - LeetHub

---
+++ 0756-pyramid-transition-matrix/0756-pyramid-transition-matrix.java
@@ -0,0 +1,42 @@
+class Solution {
+    Map<String, List<Character>> map = new HashMap<>();
+    public boolean pyramidTransition(String bottom, List<String> allowed) {
+        
+        for(String s : allowed){
+            String key = s.substring(0 , 2);
+            char top = s.charAt(2);
+
+            map.computeIfAbsent(key, k -> new ArrayList<>()).add(top);
+        }
+        return buildPyramid(bottom);
+    }
+    private boolean buildPyramid(String bottom){
+        if(bottom.length() == 1){
+            return true;
+        }
+        List<String> nextLevels = new ArrayList<>();
+        generateNextLevels(bottom, 0, new StringBuilder(), nextLevels);
+
+        for(String next : nextLevels){
+            if(buildPyramid(next)){
+                return true;
+            }
+        }
+        return false;
+    }
+    private void generateNextLevels(String bottom, int index, StringBuilder current, List<String> result){
+        if(index == bottom.length() -1){
+            result.add(current.toString());
+            return;
+        }
+        String key = bottom.substring(index, index + 2);
+        if(!map.containsKey(key)){
+            return;
+        }
+        for(char c : map.get(key)){
+            current.append(c);
+            generateNextLevels(bottom, index + 1, current, result);
+            current.deleteCharAt(current.length() - 1);
+        }
+    }
+}
