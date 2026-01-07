80d0e88435f0927fb947ea2a7d3d930f040b27b7
Shreya Gupta <shreyakgupta09@gmail.com>
2026-01-07 22:47:10 +0530

    Time: 12 ms (15.71%), Space: 51.8 MB (17.3%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        HashSet<String> seen = new HashSet<>();
+
+        for(int i = 0; i < 9; i++){
+            for(int j = 0; j < 9; j++){
+
+                char nums = board[i][j];
+                if(nums == '.') continue;
+
+                if(!seen.add(nums + "in row" + i) ||
+                !seen.add(nums + "in col" + j) ||
+                !seen.add(nums + "in box" + (i / 3) + "_" + (j / 3))){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
