3be57f57baa562d7487dc9e6487e4eb23d25f326
Shreya Gupta <shreyakgupta09@gmail.com>
2026-08-01 23:08:59 +0530

    Time: 5 ms (80.94%), Space: 45.7 MB (87.9%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,14 +1,16 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        if(x<0) 
-        return false;
-        int y=x;
-        int res = 0;
-        while(y!=0){
-            int rem = y%10;
-            res = rem + (res * 10);
-            y = y/10;
+            if(x < 0){
+                return false;
+            }
+            int original = x;
+            int reverse = 0;
+
+            while(x != 0){
+                int digit = x % 10;
+                reverse = reverse * 10 + digit;
+                x = x / 10;
+            }
+            return original == reverse;
         }
-        return res == x;
     }
-}
